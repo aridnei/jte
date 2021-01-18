9b745c027af74b70f8149f146670ac0848b5ff26
Aridnei do Carmo <aridnei@gmail.com>
2021-01-18 20:40:42 +0400

    Create to_development.groovy

---
+++ libraries/deploy/steps/to_development.groovy
@@ -0,0 +1,10 @@
+
+void call( String env_name, body ) {
+  println 'running to_env() ' + env_name
+  if ( env_name != 'dev' ) {
+    println 'skipping run because is not for Develpment environment'
+    return
+  }
+  println 'following...'
+  body()
+}
