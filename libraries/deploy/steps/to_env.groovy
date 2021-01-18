c9769c5bbb9fab4e02b8c677aba686575b234813
Aridnei do Carmo <aridnei@gmail.com>
2021-01-18 20:53:12 +0400

    Delete to_env.groovy

--- libraries/deploy/steps/to_env.groovy
+++
@@ -1,9 +0,0 @@
-void call( String env_name, body ) {
-  println 'running to_env() ' + env_name
-  if ( env_name == 'prod' ) {
-    println 'skiping...'
-    return
-  }
-  println 'following...'
-  body()
-}
