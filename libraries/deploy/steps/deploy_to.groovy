cc7d1b3cb8140ce14ae002cc89a027115f0e4363
Aridnei do Carmo <aridnei@gmail.com>
2021-01-18 21:21:41 +0400

    Update deploy_to.groovy

--- libraries/deploy/steps/deploy_to.groovy
+++ libraries/deploy/steps/deploy_to.groovy
@@ -1,5 +1,7 @@
 void call( app_env ) {
-  println 'running deploy_to() ' + app_env.long_name
-  println 'release ' + app_env.release
-  println app_env
+  stage('Deploy to: ' + app_env.short_name) {
+    println 'running deploy_to() ' + app_env.long_name
+    println 'release ' + app_env.release
+    println app_env
+  }
 }
