4d2ab12ceb8e9e693a8a920b32abc5948acf9e61
Aridnei do Carmo <aridnei@gmail.com>
2021-01-18 22:55:14 +0400

    Update pipeline1.groovy

--- pipelines/pipeline_templates/pipeline1.groovy
+++ pipelines/pipeline_templates/pipeline1.groovy
@@ -1,10 +1,10 @@
 init( "ENV: ${pipelineConfig.parameters.environment}" )
 
 to_development( pipelineConfig.parameters.environment ) {
-  prepare_ansible dev
-  prepare_package dev
-  upload_to dev
-  deploy_to dev
+  prepare_ansible_to dev
+  prepare_package_to dev
+  upload_package_to dev
+  promote_to dev
 }
 
 to_stage( pipelineConfig.parameters.environment ) {
@@ -15,8 +15,8 @@ to_stage( pipelineConfig.parameters.environment ) {
            db: {
              promote_db_to sit 
            }
-  upload_to sit
-  deploy_to sit
+  upload_package_to sit
+  promote_to sit
 }
 
 to_production( pipelineConfig.parameters.environment ) {
@@ -27,6 +27,6 @@ to_production( pipelineConfig.parameters.environment ) {
            db: {
              promote_db_to prod 
            }
-  upload_to prod
-  deploy_to prod
+  upload_package_to prod
+  promote_to prod
 }
