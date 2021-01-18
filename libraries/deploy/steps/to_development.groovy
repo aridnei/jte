0672becd8f6c3847431bd1a65094534d93fbd1e2
Aridnei do Carmo <aridnei@gmail.com>
2021-01-18 20:53:32 +0400

    Update to_development.groovy

--- libraries/deploy/steps/to_development.groovy
+++ libraries/deploy/steps/to_development.groovy
@@ -1,7 +1,6 @@
 void call( String env_name, body ) {
-  println 'running to_env() ' + env_name
   if ( env_name != 'dev' ) {
-    println 'skipping run because is not for Develpment environment'
+    println 'skipping run because is not for Development environment'
     return
   }
   println 'following...'
