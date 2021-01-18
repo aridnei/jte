24e2a4957fd1237b9ceefaf20fd5238ecc307cce
Aridnei do Carmo <aridnei@gmail.com>
2021-01-18 20:41:44 +0400

    Update to_production.groovy

--- libraries/deploy/steps/to_production.groovy
+++ libraries/deploy/steps/to_production.groovy
@@ -1,7 +1,7 @@
 void call( String env_name, body ) {
   println 'running to_env() ' + env_name
   if ( env_name != 'prod' ) {
-    println 'skipping run because is not for Develpment environment'
+    println 'skipping run because is not for Production environment'
     return
   }
   println 'following...'
