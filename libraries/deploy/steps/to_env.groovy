b41c450e895c15a764732be54801c30893c1d91c
Aridnei do Carmo <aridnei@gmail.com>
2021-01-18 20:35:42 +0400

    Update to_env.groovy

--- libraries/deploy/steps/to_env.groovy
+++ libraries/deploy/steps/to_env.groovy
@@ -1,8 +1,9 @@
-void call( def env_name ) {
+void call( String env_name, body ) {
   println 'running to_env() ' + env_name
   if ( env_name == 'prod' ) {
     println 'skiping...'
     return
   }
   println 'following...'
+  body()
 }
