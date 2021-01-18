2800f9d9e6c5fca80f8e3ac431df2bd76b6bce1c
Aridnei do Carmo <aridnei@gmail.com>
2021-01-18 20:44:42 +0400

    Update pipeline1.groovy

--- pipelines/pipeline_templates/pipeline1.groovy
+++ pipelines/pipeline_templates/pipeline1.groovy
@@ -2,6 +2,9 @@ build()
 to_development( pipelineConfig.environment ) {
   deploy_to dev
 }
+to_stage( pipelineConfig.environment ) {
+  deploy_to sit
+}
 to_production( pipelineConfig.environment ) {
   deploy_to prod
 }
